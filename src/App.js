5b5276989064c1c2c1764cfec6aa3b8c00c37f7e
Abhishek Jairath <jairath.abhishek@gmail.com>
2021-08-08 16:33:31 +0530

    rstructure layout

--- src/App.js
+++ src/App.js
@@ -4,31 +4,43 @@ import {
   Switch,
   Route,
 } from "react-router-dom";
+import { Layout } from 'antd';
+
+import './App.css';
 
 import { NavBar } from "./components/Navbar";
 import { Tasks } from "./pages/tasks";
+
+const { Content, Footer } = Layout;
 
 export default function BasicExample() {
   return (
     <>
       <Router>
-        <NavBar />
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-            <Route path="/tasks">
-              <Tasks />
-            </Route>
-          </Switch>
-        </div>
+        <Layout>
+          <NavBar />
+
+          <Content className="site-layout" style={{ padding: '0 50px', marginTop: 64 }}>
+            <div className="site-layout-background" style={{ padding: 24, minHeight: 380 }}>
+              <Switch>
+                <Route exact path="/">
+                  <Home />
+                </Route>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/dashboard">
+                  <Dashboard />
+                </Route>
+                <Route path="/tasks">
+                  <Tasks />
+                </Route>
+              </Switch>
+            </div>
+          </Content>
+
+          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+        </Layout>
       </Router>
     </>
   );
